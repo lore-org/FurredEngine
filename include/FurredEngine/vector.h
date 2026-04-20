6f9e00748abc1a7de4d069245017bf246b77e7f5
Lexi <williambush357@gmail.com>
2026-04-19 23:31:29 -0400

    partially implement FE_Vector

---
+++ include/FurredEngine/vector.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "exports.h"
+#include <stdbool.h>
+
+typedef unsigned int FE_size_t;
+
+typedef struct {
+    FE_size_t size;
+    const FE_size_t dataSize;
+    void** data;
+    FE_size_t capacity;
+} FE_Vector;
+
+FE_EXPORT FE_Vector furred_vector_create(FE_size_t size, FE_size_t dataSize);
+FE_EXPORT void furred_vector_destroy(FE_Vector* vector);
+
+FE_EXPORT void* furred_vector_at(FE_Vector* vector, FE_size_t index);
+FE_EXPORT void* furred_vector_front(FE_Vector* vector);
+FE_EXPORT void* furred_vector_back(FE_Vector* vector);
+
+FE_EXPORT void furred_vector_reserve(FE_Vector* vector, FE_size_t newCapacity);
+FE_EXPORT void furred_vector_shrink_to_fit(FE_Vector* vector); // TODO - impl
+
+FE_EXPORT void furred_vector_clear(FE_Vector* vector);
+FE_EXPORT void furred_vector_insert(FE_Vector* vector, FE_size_t index, void* data); // TODO - impl
+FE_EXPORT void furred_vector_emplace(FE_Vector* vector, FE_size_t index, void* data); // TODO - impl
+FE_EXPORT void furred_vector_erase(FE_Vector* vector, FE_size_t index); // TODO - impl
+FE_EXPORT void furred_vector_push_back(FE_Vector* vector, void* data);
+FE_EXPORT void furred_vector_pop_back(FE_Vector* vector);
+FE_EXPORT void furred_vector_resize(FE_Vector* vector, FE_size_t newSize);
+FE_EXPORT void furred_vector_swap(FE_Vector* vector, FE_Vector* otherVector); // TODO - impl
---
+++ src/vector.c
@@ -0,0 +1,109 @@
+#include <FurredEngine/vector.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define _FE_VECTOR_EXISTS_ASSERT(v) \
+    assert(v && "Vector does not exist."); \
+    assert(v->data && "Vector has already been destroyed.")
+
+
+FE_Vector furred_vector_create(FE_size_t size, FE_size_t dataSize) {
+    void** data = calloc(size, dataSize);
+    assert(data && "Vector could not be created. Does the system have enough memory?");
+
+    FE_Vector vector = {
+        .size       = size,
+        .dataSize   = dataSize,
+        .data       = data,
+        .capacity   = size
+    };
+
+    return vector;
+}
+
+void furred_vector_destroy(FE_Vector* vector) {
+    if (!vector) return;
+
+    free(vector->data);
+    vector->data = NULL;
+
+    vector->size = 0;
+    vector->capacity = 0;
+}
+
+
+void* furred_vector_at(FE_Vector* vector, FE_size_t index) {
+    _FE_VECTOR_EXISTS_ASSERT(vector);
+    assert(index < vector->size && "Vector index is out of range.");
+
+    return *(vector->data + (index * vector->dataSize));
+}
+
+void* furred_vector_front(FE_Vector* vector) {
+    return furred_vector_at(vector, 0);
+}
+
+void* furred_vector_back(FE_Vector* vector) {
+    return furred_vector_at(vector, vector->size - 1);
+}
+
+
+void furred_vector_reserve(FE_Vector* vector, FE_size_t newCapacity) {
+    // Check if space actually needs to be reserved
+    if (newCapacity <= vector->capacity) return;
+
+    const FE_size_t capacityBytes = newCapacity * vector->dataSize;
+
+    // Allocate new array
+    void** newData = realloc(vector->data, capacityBytes);
+    assert(newData && "Vector could not be expanded. Does the system have enough memory?");
+    
+    // Update vector object
+    vector->data = newData;
+    vector->capacity = newCapacity;
+}
+
+
+void furred_vector_clear(FE_Vector* vector) {
+    // Avoid using furred_vector_resize to prevent wasted computations
+    vector->size = 0;
+}
+
+void furred_vector_push_back(FE_Vector* vector, void* data) {
+    const FE_size_t oldSize = vector->size;
+    furred_vector_resize(vector, vector->size + 1);
+    // Retreive index to last item
+    // Avoid using furred_vector_back to prevent wasted computations
+    void** dataPtr = vector->data + (oldSize * vector->dataSize);
+    // Assign last item to data
+    *dataPtr = data;
+}
+
+void furred_vector_pop_back(FE_Vector* vector) {
+    assert(vector->size != 0 && "Vector has no elements to pop.");
+
+    // Avoid using furred_vector_resize to prevent wasted computations
+    vector->size--;
+}
+
+unsigned int next_pow2(unsigned int x) {
+    if (x < 2) return 2;
+    return ((unsigned int)1 << 31) >> (__builtin_clz(x) - 1);
+}
+
+void furred_vector_resize(FE_Vector* vector, FE_size_t newSize) {
+    _FE_VECTOR_EXISTS_ASSERT(vector);
+    
+    // If current capacity cannot handle newSize
+    if (newSize > vector->capacity) {
+        // Increase the current capacity to the power of 2 following newSize
+        // This is done to ensure that the larger a vector grows the less allocations it should need
+        // furred_vector_reserve incurs an extra unneeded check, but code brevity is more important here
+        furred_vector_reserve(vector, next_pow2(newSize));
+    }
+    // Else, vector does not need to be reallocated
+
+    vector->size = newSize;
+}
